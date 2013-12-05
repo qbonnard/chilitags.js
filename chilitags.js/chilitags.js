39d4b79aa02a4488f28f5ce3958d675e58759815
NaokiIbe <naokiibe@ubuntu.(none)>
2013-12-05 23:17:39 +0900

    Add javascript-side interface and modify the compilation instruction

--- chilitags.js/chilitags.js
+++ chilitags.js/chilitags.js
@@ -7687,3 +7687,17 @@ if (Module['noInitialRun']) {
 run();
 // {{POST_RUN_ADDITIONS}}
 // {{MODULE_ADDITIONS}}
+function get3dPositions (canvas) {
+    var inputBuf = Module._malloc(canvas.width*canvas.height);
+    var img = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+    var binary = new Uint8Array(img.data.length/4);
+    for(var i=0; i<binary.length; i++){
+        setValue(inputBuf+i, Math.min(0.299 * img.data[4*i] + 0.587 * img.data[4*i+1] + 0.114 * img.data[4*i+2], 255), "i8");
+    }
+    var output = Module.ccall('get3dPosition', 'string', ['number', 'number', 'number'], [inputBuf, width, height]);
+    var obj = JSON.parse(output);
+    Module._free(inputBuf);
+    return obj
+}
+Module['get3dPositions'] = get3dPositions;
+this['Chilitags'] = Module;
