7e43f768c56be18109be8f86b3ded0dd50e2e2bf
lorenzo <lorenzo@lorenzo-Vostro-3500.(none)>
2014-02-25 19:04:37 +0100

    Removed Atmosphere files. I will create new repository.

--- package.js
+++
@@ -1,8 +0,0 @@
-Package.describe({
-	summary: "Chilitags-CHILI-EPFL's JavaScript tools for augmented reality"
-});
-
-Package.on_use(function (api, where) {
- 	api.add_files('chilitags.js', 'client');
- 	api.export(['Module'], 'client');
-});
